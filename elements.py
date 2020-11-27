97ac366699a2a4426b51bebe24358649c0f67afa
Trevor <cfc603@users.noreply.github.com>
2020-11-27 15:04:10 -0500

    update Form.display
    move form into view

--- elements.py
+++ elements.py
@@ -23,6 +23,7 @@ class Element:
 class Form(Element):
 
     def display(self):
+        self.we.location_once_scrolled_into_view
         self.get_visible_inputs()[0].click()
 
     def get_inputs(self):
